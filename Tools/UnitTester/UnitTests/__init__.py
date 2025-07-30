05bab6d2976dbf9d37528273601562e7310894b8
Alex-Au <AlexXianZhenYuAu@gmail.com>
2025-07-29 22:52:23 -0400

    Add unit tests for AF5

--- Tools/UnitTester/UnitTests/__init__.py
+++ Tools/UnitTester/UnitTests/__init__.py
@@ -3,6 +3,7 @@ from .test_R7 import R7Test
 from .test_R8 import R8Test
 from .test_AF1 import AF1Test
 from .test_AF2 import AF2Test
+from .test_AF5 import AF5Test
 
 
-__all__ = ["R6Test", "R7Test", "R8Test", "AF1Test", "AF2Test"]
+__all__ = ["R6Test", "R7Test", "R8Test", "AF1Test", "AF2Test", "AF5Test"]
